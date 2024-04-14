f64a44e629cb5fc18bfb7defa714ee4418a770ea
WojciechMat <wmatsad@gmail.com>
2024-04-14 11:31:02 +0200

    update default eps

--- tokenizer_generator.py
+++ tokenizer_generator.py
@@ -74,7 +74,7 @@ class OneTimeTokenizerFactory(TokenizerFactory):
 
     @staticmethod
     def select_parameters() -> dict:
-        eps = st.number_input(label="eps - minimal time shift value", value=0.1, format="%0.3f")
+        eps = st.number_input(label="eps - minimal time shift value", value=0.01, format="%0.3f")
         n_velocity_bins = st.number_input(label="n_velocity_bins", value=32)
         return {"eps": eps, "n_velocity_bins": n_velocity_bins}
 
