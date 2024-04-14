e572740479bfd641a4a96335910f7a05b3bc3466
WojciechMat <wmatsad@gmail.com>
2024-04-14 12:19:00 +0200

    use token_to_dt

--- midi_tokenizers/no_loss_tokenizer.py
+++ midi_tokenizers/no_loss_tokenizer.py
@@ -150,7 +150,7 @@ class NoLossTokenizer(MidiTokenizer):
         current_velocity = 0
         for token in tokens:
             if "s" in token:
-                dt: float = eval(token[:-1])
+                dt: float = self.token_to_dt(token)
                 current_time += dt
             if "VELOCITY" in token:
                 # velocity should always be right before NOTE_ON token
