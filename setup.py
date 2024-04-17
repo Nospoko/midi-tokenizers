5ea6b1ddb1dfecf461866ad002195e7f25de716f
WojciechMat <wmatsad@gmail.com>
2024-04-17 13:12:35 +0200

    make it a python package

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name="midi-tokenizers",
+    py_modules=[
+        'midi_tokenizers', 
+        'midi_trainable_tokenizers', 
+        'midi_quantizers', 
+        'artifacts', 
+        'scripts', 
+        'object_generators',
+    ],
+    install_requires=[
+        "datasets~=2.17.1",
+        "fortepyan>=0.2.8",
+        "numpy~=1.26.3",
+        "pandas~=2.1.4",
+        "PyYAML~=6.0.1",
+    ],
+)
