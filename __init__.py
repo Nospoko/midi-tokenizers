f7b962f3e3d88e2e047a120acd3807f71367265c
WojciechMat <wmatsad@gmail.com>
2024-04-22 21:58:22 +0200

    init.py

---
+++ __init__.py
@@ -0,0 +1,15 @@
+from midi_tokenizers.midi_tokenizer import MidiTokenizer
+from midi_tokenizers.no_loss_tokenizer import NoLossTokenizer
+from midi_tokenizers.one_time_tokenizer import OneTimeTokenizer
+from midi_trainable_tokenizers.bpe_tokenizer import BpeMidiTokenizer
+from midi_tokenizers.quantized_midi_tokenizer import QuantizedMidiTokenizer
+from midi_trainable_tokenizers.trainable_tokenizer import MidiTrainableTokenizer
+
+__all__ = [
+    "MidiTokenizer",
+    "NoLossTokenizer",
+    "OneTimeTokenizer",
+    "QuantizedMidiTokenizer",
+    "MidiTrainableTokenizer",
+    "BpeMidiTokenizer",
+]
