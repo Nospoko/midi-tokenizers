40c958957501e02d4dc1e42f49e52a7153ca7067
WojciechMat <wmatsad@gmail.com>
2024-04-24 00:05:35 +0200

    update project

--- __init__.py
+++ __init__.py
@@ -3,6 +3,7 @@ from midi_tokenizers.no_loss_tokenizer import NoLossTokenizer
 from midi_tokenizers.one_time_tokenizer import OneTimeTokenizer
 from midi_trainable_tokenizers.bpe_tokenizer import BpeMidiTokenizer
 from midi_tokenizers.quantized_midi_tokenizer import QuantizedMidiTokenizer
+from midi_trainable_tokenizers.awsome_midi_tokenzier import AwesomeMidiTokenizer
 from midi_trainable_tokenizers.trainable_tokenizer import MidiTrainableTokenizer
 
 __all__ = [
@@ -12,4 +13,5 @@ __all__ = [
     "QuantizedMidiTokenizer",
     "MidiTrainableTokenizer",
     "BpeMidiTokenizer",
+    "AwesomeMidiTokenizer",
 ]
